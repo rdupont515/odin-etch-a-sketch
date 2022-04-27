ed4a3afe6ced40671a1a4bee99256e6652997aea
Robert <rdupont515@gmail.com>
2022-04-27 14:17:49 -0500

    Creates a 16x16 grid of boxes, using flexbox and JavaScript.

--- js-file.js
+++ js-file.js
@@ -2,8 +2,8 @@
 
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 33; i++) {
+for (let i = 0; i < 256; i++) {
 const square = document.createElement('div');
 square.classList.add('square');
 container.appendChild(square);
-}//creates 32 squares, but they are on top of each other
+}//creates 256 squares
