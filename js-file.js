5ded4429aa671088bae66ff2ebd27d163dc6c88d
Robert <rdupont515@gmail.com>
2022-04-27 13:49:52 -0500

    Updates the comment information.

--- js-file.js
+++ js-file.js
@@ -6,4 +6,4 @@ for (let i = 0; i < 33; i++) {
 const square = document.createElement('div');
 square.classList.add('square');
 container.appendChild(square);
-}//creates 32 squares, but they are on top of each other
+}//creates 32 squares, but they are inline
