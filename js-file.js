df3d7340c80f0421840a7456e8d1bd195e8d3303
Robert <rdupont515@gmail.com>
2022-04-28 09:36:14 -0500

    Adds an event listener for button click and a prompt that requires a number less than or equal to 100.

--- js-file.js
+++ js-file.js
@@ -2,13 +2,21 @@
 
 const container = document.querySelector('#container');
 
+const btn = document.querySelector('button');
+btn.addEventListener('click', (e) => {
+    let n = parseInt(prompt('Please choose the number or squares per side.', "0"), 10);
+    if (n > 100) {
+        alert("Please choose a smaller number.");
+} //sets a limit on user input
+    else console.log(n);
+});//adds an event listener for button click
+
 for (let i = 0; i < 256; i++) {
 const square = document.createElement('div');
 square.classList.add('square');
 
 square.addEventListener('mouseenter', (e) => {
    e.target.style.background = 'red';
-    console.log("Good Job.");
   });
 container.appendChild(square);
 }//creates 256 squares that change color when the mouse passes over them
