22036b885428c19af906b4d0f1021347bf62e3eb
Robert <rdupont515@gmail.com>
2022-04-28 08:39:42 -0500

    Adds an event listener to change the color of the squares as the mouse passes over them.

--- js-file.js
+++ js-file.js
@@ -5,5 +5,11 @@ const container = document.querySelector('#container');
 for (let i = 0; i < 256; i++) {
 const square = document.createElement('div');
 square.classList.add('square');
+
+square.addEventListener('mouseenter', (e) => {
+   e.target.style.background = 'red';
+    console.log("Good Job.");
+  });
 container.appendChild(square);
-}//creates 256 squares
+}//creates 256 squares that change color when the mouse passes over them
+
