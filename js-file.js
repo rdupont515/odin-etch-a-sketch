f0547d5ff521597dfcc80fe101bc9805569bbbcd
Robert <rdupont515@gmail.com>
2022-05-05 13:53:08 -0500

    Creates a 16x16 grid that can be modified.

--- js-file.js
+++ js-file.js
@@ -1,9 +1,23 @@
 //Javascript
 
-const container = document.querySelector('#container');
+function populateBoard(size){
+    const board = document.querySelector('.board');
 
-for (let i = 0; i < 33; i++) {
-const square = document.createElement('div');
-square.classList.add('square');
-container.appendChild(square);
-}//creates 32 squares, but they are inline
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let amount = size * size
+    for (let i = 0; i < amount; i++){
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'blue';
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
+
+function changeSize(input){
+populateBoard(input);
+}
+
+populateBoard(16);
