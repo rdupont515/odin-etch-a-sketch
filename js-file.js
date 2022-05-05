fb33190af1adccac37fbedc9c4da360c5505e3f1
Robert <rdupont515@gmail.com>
2022-05-05 15:21:55 -0500

    Removes unwanted items from inex.html.     Combines the previously added code so that selecting the button will initiate the functions, removing any previous grids and creating a new one based on user input.

--- js-file.js
+++ js-file.js
@@ -1,10 +1,12 @@
 //Javascript
 
+//creates a square grid that can be drawn on and removes any previous grids
 function populateBoard(size){
     const board = document.querySelector('.board');
-
     let squares = board.querySelectorAll('div');
+
     squares.forEach((div) => div.remove());
+
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
@@ -13,34 +15,31 @@ function populateBoard(size){
         let square = document.createElement('div');
         square.style.backgroundColor = 'blue';
         board.insertAdjacentElement("beforeend", square);
+        square.addEventListener('mouseenter', (e) => {
+        e.target.style.background = 'red';
+        });
     }
 }
 
+//function to make changes
 function changeSize(input){
 populateBoard(input);
 }
 
+//creates a 16x16 grid to start the board
 populateBoard(16);
+
+//whenever the button is selected a prompt allows for a value to be selected and used to change the grid
 const btn = document.querySelector('button');
 btn.addEventListener('click', (e) => {
     let n = prompt('Please choose the number of squares per side.', "0");
-    document.getElementById('container').style.width = n + "em";//the value of user input is used to adjust the width
-    document.getElementById('container').style.height = n + "em";//the value of user input is used to adjust the height
     
-    n = parseInt(n, 10);//converts the n string to a number for further use
+    //converts the n string to a number for further use
+    n = parseInt(n, 10);
     
-    if ((n > 0) && (n <= 100)) {//tests the value of n
-        for (let a = 0; a < n; a++){
-        console.log(n);//for reference
-            for (i = 0; i < n; i++){//creates a grid
-                const square = document.createElement('div');
-                square.classList.add('square');
+    //tests the value of n
+    if ((n > 0) && (n <= 100)) {
+       changeSize(n);
+            } else alert('Choose a number between 0 and 100.')
+})
 
-                square.addEventListener('mouseenter', (e) => {
-                    e.target.style.background = 'red';
-                });
-                container.appendChild(square);
-            }
-            }
-        };
-        })
